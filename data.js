43ca36aa2c27f2c1058e96614f281e0130de6355
Mohammed Mudassir <mohammedmudassir687687@gmail.com>
2022-03-02 23:02:26 -0500

    tiktok server commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url: "https://v16m-webapp.tiktokcdn-us.com/39a6c6b1cd9b6553e10f0acc770e7ebb/6220357f/video/tos/useast2a/tos-useast2a-pve-0068/ab49dec5b50948bd827916f8dbdadbfa/?a=1988&br=2424&bt=1212&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThoKdcDXq&l=202203022125430101131352071A18811B&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MztpOzg6ZjNsOTMzNzczM0ApZGU3aWVnNGQ4NzY5ODNoZWcubHBwcjRvbmpgLS1kMTZzczQtLzQ0YTIvNTAwMGNhLzQ6Yw%3D%3D&vl=&vr=",
+        channel: "mudassir",
+        description: "Sup Sup Sup",
+        song: "O Rabb vi khel hai khele",
+        likes: "123",
+        messages: "456",
+        shares: "789"
+    },
+    {
+        url: "https://v16m-webapp.tiktokcdn-us.com/27d08dbf81ce3c45fa85bcaabd0e0f00/622068ea/video/tos/useast5/tos-useast5-ve-0068c003-tx/1b2b9caa8bcc459cb1df68f2b7a3afc4/?a=1988&br=2322&bt=1161&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7Th8MACDXq&l=202203030106070101130060862248D5E1&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2o8N2c6ZmQ3OjMzZzczNEApOGg2NGdlZmQ7Nzs2PDVkOWdtL2VucjQwNXJgLS1kMS9zc2NjYWNjYGEtLWFjNmJiYDA6Yw%3D%3D&vl=&vr=",
+        channel: "mudassir",
+        description: "Sup Sup Sup",
+        song: "O Rabb vi khel hai khele",
+        likes: "123",
+        messages: "456",
+        shares: "789"
+    }
+];
+
+export default data;
